243b8e98a60bd658d18c0f94b2c50bb4ffa52b8f
Albert Company <albertdc1984@gmail.com>
2024-04-16 00:47:45 +0200

    Modified a sofa on  map2.png

--- public/modern_sprite_sheet__guns__games__instruments_by_andersonevil_df1ddyf.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="modern_sprite_sheet__guns__games__instruments_by_andersonevil_df1ddyf" tilewidth="16" tileheight="16" tilecount="512" columns="32">
- <image source="../../../Downloads/modern_sprite_sheet__guns__games__instruments_by_andersonevil_df1ddyf.png" width="527" height="264"/>
-</tileset>
